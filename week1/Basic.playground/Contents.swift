446bb089b55b29b7089c41b53a7c18b13dbce1e1
BenTee <77099990+Bennacci@users.noreply.github.com>
2021-02-23 18:38:30 +0800

    add basic.playground

---
+++ week1/Basic.playground/Contents.swift
@@ -0,0 +1,54 @@
+// 1.
+let Pi = 3.14159
+
+
+//2.
+var x = 40.0
+var y = 20.0
+var average = (x+y)/2
+
+
+//3.
+let Ans3_1 = "Set both x and y in Q2 to double"
+average = 65.0
+
+let Ans3_2 = "Int division results int, and double results double."
+var intDivision = 10/3
+var doubleDivision = 10.0/3.0
+
+
+//4.
+var flag:Bool = true
+var inputString: String = "Hello world."
+let bitsInBite: Int = 8
+let averageScore: Double = 86.8
+
+
+//5.
+var salery = 22000
+var raise = 28000
+salery += raise
+
+//6.
+let Ans6 = "Equality operation in Swift is =="
+
+//7.
+var a = 10
+var b = 3
+var remain = a%b
+
+
+//8.
+let Ans8 = "let declares a constant, and var declares a variable"
+
+
+//9.
+let Ans9 = "Constant and variable names can contain almost any character, including Unicode characters; Constant and variable names can’t contain whitespace characters, mathematical symbols, arrows, private-use Unicode scalar values, or line- and box-drawing characters. Nor can they begin with a number, although numbers may be included elsewhere within the name. If giving a constant or variable the same name as a reserved Swift keyword, surround the keyword with backticks (`) when using it as a name. However avoid using keywords unless absolutly no choice"
+
+
+//10.
+let Ans10 = "Type inference in Swift, is Swift automaticaly declaring the type of a constant or variable whenever we assign a literal value (or value) to it. This leads to less type declarations than languages such as C or Obj-C."
+
+
+//11.
+let Ans11 = "The first line has set the var as a Int. We can't change the value from a Int to a String once its declared"
