ef48dbb7cd904836be00c8ffdaa4fc83d5b2d65f
BenTee <77099990+Bennacci@users.noreply.github.com>
2021-03-18 21:55:22 +0800

    week 4 Q2 redo

--- week4/odd or not closure.playground/Contents.swift
+++ week4/odd or not closure.playground/Contents.swift
@@ -1,8 +1,32 @@
+
+// Closure
+
+let oddIdentifierClosure = { (val: Int) -> (Bool) in
+  if val % 2 == 1 {
+    return true
+  } else {
+    return false
+  }
+}
+oddIdentifierClosure(9)
+
+
+
+
+
+//------------------------------------------------------------
+
+// .filter
+var numbers = Array(1...9)
+let oddNumbers = numbers.filter ({ $0 % 2 == 1})
+oddNumbers
+
+// Function
 func isOdd(val: Int) -> Bool {
   return val % 2 != 0
 }
-
-
 //testing:
 isOdd(val: 2)
 isOdd(val: 5)
+
+
